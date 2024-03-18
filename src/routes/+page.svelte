eb3d0f5b32a60f6295eb5d85a049c29301adf991
kavidey <kaviasher@gmail.com>
2024-03-17 17:51:10 -0700

    First website version working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    const objects = ['airplane', 'car', 'drawer', 'orange', 'sand'];
+    const editedExtension = '-edited';
+    const base_img_path = 'imgs/';
+    const object_filenames = objects.map((object) => base_img_path + object + '.png');
+    const object_filenames_edited = objects.map((object) => base_img_path + object + editedExtension + '.jpeg');
+</script>
+{#each objects as object, i}
+    <div>
+        <img src={object_filenames[i]} alt={object} />
+        <img src={object_filenames_edited[i]} alt={object} />
+    </div>
+{/each}
